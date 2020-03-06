49ca5fc447e5a13b66a0baf381ab5abf628f4ae0
Pau Joan <pregonipaw@hotmail.com>
2020-03-06 19:07:46 +0100

    cambios color picker, bugs, etc

--- eventos.jsx
+++ eventos.jsx
@@ -2,64 +2,71 @@
 document.addEventListener("mousedown", dibujarOn);
 document.addEventListener("mousemove", dibujando);
 document.addEventListener("mouseup", dibujarOff);
-
+document.getElementById("colorPicker");
 // CANVAS
 var cuadrito = document.getElementById("area_dibujo");
 var papel = cuadrito.getContext('2d');
 
-//
+
+papel.lineWidth = 3;
+papel.lineJoin = 'round';
+papel.lineCap = 'round';
+papel.fillStyle = 'white';
+// CUADRADO DIBUJADO EN CANVAS
+
+function dibujar(col = "red", i1, i2, l1, l2, lienzo) {
+    // lienzo.beginPath();
+    lienzo.strokeStyle = document.getElementById("colorPicker").value;
+    lienzo.lineTo(l1, l2);
+    lienzo.stroke();
+}
+
+
 var x, y, color;
 var quiereDibujo = false;
 
-function colorin(col)
-{
-    color = col;
-}
 
-function goma(col)
-{
-    color = col;
-}
-
-function dibujarOn()
+function dibujarOn(dibu)
 {
     quiereDibujo = true;
+    papel.beginPath();
+    cuadrito.style.cursor = 'wait'
 }
 
 function dibujando(dibu)
 {
-    x = dibu.clientY - 113;
-    y = dibu.clientX - 10;
+    
+    x = dibu.clientY - 189;
+    y = dibu.clientX - 208;
     if (quiereDibujo == true)
     {
         if (color == "white") {
-            dibujar(color, y-1, x-1, y+1, x+1, papel, 25);  
+            // GOMA DE BORRAR
+            dibujar(color, y, x , y , x, papel, 25);  
+            
         }
         else
         {
-            dibujar(color, y-1, x-1, y+1, x+1, papel);
+            dibujar(color, y, x, y, x, papel);     
         }
     }
 }
 
+function fill() {
+    papel.fillRect(0, 0, cuadrito.width, cuadrito.height);
+}
+
+
 function dibujarOff()
 {
     quiereDibujo = false;
+    cuadrito.style.cursor = 'auto'
 }
 
-dibujar("black", 2, 2, 2, 300, papel);
-dibujar("black", 2, 300, 300, 300, papel);
-dibujar("black", 300, 300, 300, 2, papel);
-dibujar("black", 300, 2, 2, 2, papel);
 
-
-function dibujar(col = "red", i1, i2, l1, l2, lienzo, wd = 2) {
-    lienzo.beginPath();
-    lienzo.strokeStyle = col;
-    lienzo.lineWidth = wd;
-    lienzo.moveTo(i1, i2);
-    lienzo.lineTo(l1, l2);
-    lienzo.stroke();
-    lienzo.closePath();
+function colorin(col)
+{
+    color = col
+    papel.strokeStyle = col
 }
 
