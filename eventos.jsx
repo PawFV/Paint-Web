3a53ffa6936baf06f04c32dc1ad4ab5ba4c0b8ff
Pau Joan <pregonipaw@hotmail.com>
2020-03-06 23:48:12 +0100

    Mejoradisimo

--- eventos.jsx
+++ eventos.jsx
@@ -2,71 +2,104 @@
 document.addEventListener("mousedown", dibujarOn);
 document.addEventListener("mousemove", dibujando);
 document.addEventListener("mouseup", dibujarOff);
-document.getElementById("colorPicker");
+var color = document.getElementById("colorPicker")
 // CANVAS
 var cuadrito = document.getElementById("area_dibujo");
 var papel = cuadrito.getContext('2d');
+var pen = document.getElementById("pen")
+var goma = document.getElementById("gomaIco")
+cuadrito.addEventListener("pointermove", pincelOn)
+cuadrito.addEventListener("pointerout", pincelOff)
+
+var x, y
+var lastColor = color.value
+var quiereDibujo = false
+var quiereBorrar = false
 
 
-papel.lineWidth = 3;
-papel.lineJoin = 'round';
-papel.lineCap = 'round';
-papel.fillStyle = 'white';
-// CUADRADO DIBUJADO EN CANVAS
 
-function dibujar(col = "red", i1, i2, l1, l2, lienzo) {
-    // lienzo.beginPath();
-    lienzo.strokeStyle = document.getElementById("colorPicker").value;
-    lienzo.lineTo(l1, l2);
-    lienzo.stroke();
+function dibujar(col = 'red', l1, l2, lienzo, lw = 3, lj = 'round', lc = 'round') {
+    lienzo.strokeStyle = col
+    lienzo.lineWidth = lw
+    lienzo.lineJoin = lj
+    lienzo.lineCap = lc
+    lienzo.lineTo(l1, l2)
+    lienzo.stroke()
+    console.log(lienzo.lineWidth)
 }
 
-
-var x, y, color;
-var quiereDibujo = false;
-
-
-function dibujarOn(dibu)
+function dibujarOn()
 {
-    quiereDibujo = true;
-    papel.beginPath();
-    cuadrito.style.cursor = 'wait'
+    quiereDibujo = true
+    papel.beginPath()
 }
 
-function dibujando(dibu)
+// PINCEL/GOMA
+function pincelOff()
 {
+    document.body.style.cursor = 'auto'
+    if (!quiereBorrar) {
+        pen.style.visibility = 'hidden'
+    } else {
+        goma.style.visibility = 'hidden'
+    }
+}
+function pincelOn() {
+    document.body.style.cursor = 'none'
     
-    x = dibu.clientY - 189;
-    y = dibu.clientX - 208;
-    if (quiereDibujo == true)
-    {
-        if (color == "white") {
-            // GOMA DE BORRAR
-            dibujar(color, y, x , y , x, papel, 25);  
-            
-        }
-        else
-        {
-            dibujar(color, y, x, y, x, papel);     
-        }
+    if (!quiereBorrar) {
+        pen.style.top = `${x + 130}` + 'px'
+        pen.style.left = `${y + 204}` + 'px'
+        pen.style.visibility = 'visible'
+        
+    } else {
+        goma.style.top = `${x + 130}` + 'px'
+        goma.style.left = `${y + 204}` + 'px'
+        goma.style.visibility = 'visible'
     }
 }
 
-function fill() {
-    papel.fillRect(0, 0, cuadrito.width, cuadrito.height);
+function borrar(condicion) {
+    quiereBorrar = condicion
+    color.value = lastColor
+    if (quiereBorrar)
+    {
+        lastColor = color.value;
+        color.value = '#ffffff'    
+    }
+    else {
+        color.value = lastColor
+    }
 }
 
 
+function dibujando(dibu)
+{
+    x = dibu.clientY - 140;
+    y = dibu.clientX - 208;
+   
+    if (quiereDibujo == true)
+    {   
+        if (color.value == "#ffffff") {
+            // GOMA DE BORRAR
+            dibujar(color.value, y-1 , x, papel, 8,'square','square'); 
+        }
+        else
+        {
+            dibujar(color.value, y, x, papel);    
+        }
+    }
+}
 function dibujarOff()
 {
-    quiereDibujo = false;
-    cuadrito.style.cursor = 'auto'
+    quiereDibujo = false 
+}
+
+function fill() {
+    papel.fillStyle = color.value
+    papel.fillRect(0, 0, cuadrito.width, cuadrito.height)
 }
 
 
-function colorin(col)
-{
-    color = col
-    papel.strokeStyle = col
-}
+
 
